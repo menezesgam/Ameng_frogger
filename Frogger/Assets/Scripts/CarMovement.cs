978c542492ad34d32b6d79887f9786a8e680a860
moika <menezesgam@gmail.com>
2015-04-02 01:25:41 -0300

    aaa

---
+++ Frogger/Assets/Scripts/CarMovement.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CarMovement : MonoBehaviour
+{
+
+		private Vector3 endPoint;
+		public float speed = 3f;
+
+		// Update is called once per frame
+		void Update ()
+		{
+				if (endPoint != null) {
+						transform.position = Vector3.MoveTowards (transform.position, endPoint, speed * Time.deltaTime);
+						if (transform.position == endPoint) {
+								Destroy (gameObject);
+						}
+				}
+		}
+
+		public void EndPoint (Vector3 position)
+		{
+				endPoint = position;
+		}
+}
---
+++ Frogger/Assets/Scripts/FrogCollision.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FrogCollision : MonoBehaviour
+{
+
+		private Vector3 startPosition;
+		// Use this for initialization
+		void Start ()
+		{
+				startPosition = transform.position;
+		}
+
+		public void OnTriggerEnter2D (Collider2D col)
+		{
+				transform.position = startPosition;
+		}
+}
